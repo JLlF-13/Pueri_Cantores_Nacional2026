38310a2673352845aa0cea4b6855ca0f63c4f5c4
JLlF-13 <joan.lluch.fonoll@gmail.com>
2026-09-09 18:29:00 +0200

    Problems fix

--- pueris/src/components/header.jsx
+++ pueris/src/components/header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
         {/* Logo */}
         <a href="/" className="flex items-center gap-3">
           <img
-            src="/logo.png"
+            src={`${process.env.PUBLIC_URL}/logo.png`}
             alt="Pueri Cantores"
             className="h-12 w-auto"
           />
